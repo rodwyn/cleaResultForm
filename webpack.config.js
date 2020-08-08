4f8a61c1575f7b6a7167967fbaa98bd04cb5e655
Rodwyn <rodwynmoreno@gmail.com>
2020-08-07 19:52:09 -0500

    Added fonts rule.

--- webpack.config.js
+++ webpack.config.js
@@ -23,6 +23,17 @@ module.exports = {
         exclude: /node_modules/,
         use: ['style-loader', 'css-loader?sourceMap=true', 'sass-loader']
       },
+      {
+        test: /\.(ttf|woff|woff2)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'file-loader',
+          options: {
+            name: 'fonts/[name].[ext]',
+            outputPath: 'fonts',
+          },
+        },
+      },
     ],
   },
   resolve: {
